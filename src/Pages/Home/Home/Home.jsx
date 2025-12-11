df62300b4068caee2edcecc113b40b400bb6b62c
Ashiqweb123 <rabiulazim30@gmail.com>
2025-12-11 18:55:40 +0600

    brand-add

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "./Banner/Banner";
 import HowWorks from "./HowWorks";
+import PromoSection from "./PromoSection/PromoSection";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <HowWorks></HowWorks>
+      <PromoSection></PromoSection>
     </div>
   );
 };
